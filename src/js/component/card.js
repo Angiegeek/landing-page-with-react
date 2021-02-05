f12bb5d26d153af524d626e15880393380332ca3
Angiegeek <avm03021993@gmail.com>
2021-02-05 01:00:06 +0000

    Envio trabajo dia 12 am react

---
+++ src/js/component/card.js
@@ -0,0 +1,80 @@
+import React from "react";
+
+export const Card = () => {
+	return (
+		<div className="card-deck">
+			<div className="card cardstyle">
+				<img
+					src="https://www.ukuta.com/assets/24585841/img/500x400.png"
+					className="card-img-top"
+					alt="..."
+				/>
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Nunc euismod iaculis quam vitae mollis. Praesent rhoncus
+						aliquet suscipit.
+					</p>
+					<a href="#" className="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+			<div className="card cardstyle">
+				<img
+					src="https://www.ukuta.com/assets/24585841/img/500x400.png"
+					className="card-img-top"
+					alt="..."
+				/>
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Nunc euismod iaculis quam vitae mollis. Praesent rhoncus
+						aliquet suscipit.
+					</p>
+					<a href="#" className="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+			<div className="card cardstyle">
+				<img
+					src="https://www.ukuta.com/assets/24585841/img/500x400.png"
+					className="card-img-top"
+					alt="..."
+				/>
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Nunc euismod iaculis quam vitae mollis. Praesent rhoncus
+						aliquet suscipit.
+					</p>
+					<a href="#" className="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+			<div className="card cardstyle">
+				<img
+					src="https://www.ukuta.com/assets/24585841/img/500x400.png"
+					className="card-img-top"
+					alt="..."
+				/>
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Nunc euismod iaculis quam vitae mollis. Praesent rhoncus
+						aliquet suscipit.
+					</p>
+					<a href="#" className="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
---
+++ src/js/component/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Footer = () => {
+	return (
+		<div clasName="row">
+			<p className="text center footerstyle">
+				Copyright © Your Website 2021
+			</p>
+		</div>
+	);
+};
